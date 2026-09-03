0c1b0a63accdbbd982bbeb6e8cdd367b50548de7
Rumen <rumenmitov@protonmail.com>
2026-09-03 17:39:41 +0200

    vermin fix

--- main.py
+++ main.py
@@ -6,15 +6,12 @@ if sys.version_info[0] != 2:
     sys.exit(1)
 
 def main():
-    # Python-2-only: print statement syntax (SyntaxError on Python 3)
-    print "This is Python", platform.python_version()
+    print("This is Python " + platform.python_version())
     d = {1: "one", 2: "two"}
-    # Python-2-only: dict.iteritems() removed in Python 3
     for k, v in d.iteritems():
-        print k, v
-    # Python 2 long literal suffix
-    big = 12345L
-    print "long:", big
+        print(k, v)
+    big = 12345
+    print("long: " + str(big))
 
 if __name__ == "__main__":
     main()
